48f6d6cec16e2da413ba2a420b6e94bdc8366244
Fernando Ofarrill <fernando.ofarrill@mykenzie.snhu.edu>
2022-07-12 17:59:37 -0400

    Added try/catch in main method

--- src/main/java/com/kenzie/app/Main.java
+++ src/main/java/com/kenzie/app/Main.java
@@ -28,45 +28,58 @@ public class Main {
      */
     public static final String URL = "https://jservice.kenzie.academy/api/clues";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         //Write main execution code here
 
         //Declarations of variables to track points and counter for the for loop
-        int points = 0;
-        int counter = 0;
-        //Declaration of random and scanner
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        int randomId = random.nextInt(101);
+        try {
+            int points = 0;
+            int counter = 0;
+            //Declaration of random and scanner
+            Random random = new Random();
+            Scanner scanner = new Scanner(System.in);
+            int randomId = random.nextInt(101);
 
 
-        //Pre game info explaining the game system
-        System.out.println("Hello! Welcome to the \"Restless Neuron Game!\"" + "\n");
-        System.out.println("The game consists in answering as many questions correctly.");
-        System.out.println("For every question that is correct you get a point, for a total of 10 questions." + "\n" +
-                "Lets see what you got. Ready?");
-        System.out.println("Type Y to continue");
-        scanner.nextLine();
-        System.out.println("First Question");
+            //Pre game info explaining the game system
+            System.out.println("Hello! Welcome to the \"Restless Neuron Game!\"" + "\n");
+            System.out.println("The game consists in answering as many questions correctly.");
+            System.out.println("For every question that is correct you get a point, for a total of 10 questions." + "\n" +
+                    "Lets see what you got. Ready?");
+            System.out.println("Type Y to continue");
+            scanner.nextLine();
+            System.out.println("First Question");
 
             //Main execution of program using loops and scanners
             for (Clue clue : mapper(URL).getClues()) {
                 while (clue.getId() == randomId) {
                     System.out.println("Category: " + clue.getCategory().getTitle() + "\n"
-                            + "Question: " + clue.getQuestion()+ "\n");
+                            + "Question: " + clue.getQuestion() + "\n");
                     System.out.println("Type your answer");
                     String answer = scanner.nextLine();
                     counter++;
 
-                    if (answer.equalsIgnoreCase(clue.getAnswer())) {
-                        System.out.println("Correct! You get a point!");
-                        points++;
-                        System.out.println("Points: " + points + "\n");
-                    } else {
+                    //Try/catch to check that string is not empty or white spaces also repeats the same question until a valid answer is entered
+                    try {
+                        if(answer.isEmpty() || answer.isBlank()){
+                            throw new RuntimeException();
+                        }
+                    }catch (RuntimeException e){
+                        counter--;
+                        randomId--;
+                        System.out.println( "Please enter a valid answer!"+ "\n");
+                    }
+
+                   if (answer.equalsIgnoreCase(clue.getAnswer())) {
+                       System.out.println("Correct! You get a point!");
+                       points++;
+                       System.out.println("Points: " + points + "\n");
+                   } else if(!answer.equalsIgnoreCase(clue.getAnswer()) && !answer.isEmpty() && !answer.isBlank()){
                         System.out.println("Wrong answer. The correct answer is: " + clue.getAnswer() + "\n");
                         System.out.println("Points: " + points);
                     }
                     randomId++;
+
                 }
                 if (counter == 10) {
                     break;
@@ -79,6 +92,9 @@ public class Main {
                 System.out.println("You got a total of: " + points + " points");
                 System.out.println("Better luck next time :(");
             }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
 
     }
 
